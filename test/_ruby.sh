aaa6d3e5bcd3753410dc64f96663a7fb8af09ac3
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-11-16 14:05:10 +0530

    Update _ruby.sh

--- test/_ruby.sh
+++ test/_ruby.sh
@@ -5,22 +5,18 @@ echo "rvm ls"
 rvm ls
 printf "\n"
 
-echo "rvm use jruby-1.7.27"
-rvm use jruby-1.7.27
+echo "rvm use jruby-9.2.3.0"
+rvm use jruby-9.2.3.0
 printf "\n"
 
-echo "rvm use jruby-9.2.0.0"
-rvm use jruby-9.2.0.0
+echo "rvm use 2.3.8"
+rvm use 2.3.8
 printf "\n"
 
-echo "rvm use 2.3.7"
-rvm use 2.3.7
+echo "rvm use 2.4.5"
+rvm use 2.4.5
 printf "\n"
 
-echo "rvm use 2.4.4"
-rvm use 2.4.4
-printf "\n"
-
-echo "rvm use 2.5.1"
-rvm use 2.5.1
+echo "rvm use 2.5.3"
+rvm use 2.5.3
 printf "\n"
